c693824cfb59c5c9cb9294ada266fbad9c03890d
jongwon <90269036+jonnygim@users.noreply.github.com>
2022-09-13 16:11:22 +0900

    Create correct_parenthesis.py

---
+++ src/programmers/stack+queue/correct_parenthesis.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = []
+    
+    for i in s:
+        if i == '(':
+            answer.append(i)
+        else:
+            if answer == []:
+                return False
+            else:
+                answer.pop()
+                
+        
+    return answer == []
